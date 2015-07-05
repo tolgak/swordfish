d8aac803158d760f7b717241860211a7af4ae9c3
Tolga Kurkcuoglu <tolga.kurkcuoglu@gmail.com>
2015-07-05 04:31:59 +0300

    boiler plate
    identity 2.0
    sb admin 3 bootstrap template

---
+++ Swordfish.Web/Swordfish.Web/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace IdentitySample.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult Index2()
+    {
+      return View();
+    }
+
+    public ActionResult About()
+    {
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      return View();
+    }
+  }
+}
